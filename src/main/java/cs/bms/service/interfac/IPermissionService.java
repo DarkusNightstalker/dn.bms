9fc569756b7022520da386f53d0ad123ea43121c
Darkus Nightmare <Darkus Nightmare@Jhoan-PC>
2017-08-29 19:11:28 -0500

    ISM Service Update

--- src/main/java/cs/bms/service/interfac/IPermissionService.java
+++ src/main/java/cs/bms/service/interfac/IPermissionService.java
@@ -4,18 +4,15 @@ import cs.bms.model.Permission;
 import cs.bms.model.PermissionCategory;
 import cs.bms.model.User;
 import gkfire.hibernate.generic.interfac.IGenericService;
+
 import java.util.List;
 
-public abstract interface IPermissionService
-  extends IGenericService<Permission, Integer>
-{
-  public abstract List<Permission> getBy(PermissionCategory paramPermissionCategory);
-  
-  public abstract List<String> getPermissionCodeFromUser(User paramUser);
+public interface IPermissionService
+        extends IGenericService<Permission, Integer> {
+    public List<Permission> getBy(PermissionCategory paramPermissionCategory);
+
+    public List<String> getPermissionCodeFromUser(User paramUser);
+
+    public Integer getIdByCode(String code);
 }
 
-
-/* Location:              D:\Proyectos\cs.bms.minisol.web-1.0.1\WEB-INF\lib\cs.bms-1.0.jar!\cs\bms\service\interfac\IPermissionService.class
- * Java compiler version: 8 (52.0)
- * JD-Core Version:       0.7.1
- */
